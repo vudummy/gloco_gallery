935ba837b654b53aa70405fa4b20bf6e752384c5
vutran47 <vutran.1710@gmail.com>
2017-04-12 02:39:45 +0700

    Add css-loader and react-motion

--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,7 @@ module.exports = {
   module: {
     loaders: [
       { test: /\.jsx?$/, loader: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.css$/, loader: "style-loader!css-loader" }
     ]
   },
   plugins: [HtmlWebpackPluginConfig]

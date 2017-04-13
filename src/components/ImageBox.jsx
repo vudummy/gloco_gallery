a5dc6f7330a1030e255e2ff3606598f0ee6d75b0
vutran47 <vutran.1710@gmail.com>
2017-04-13 08:32:55 +0700

    Showing error on imgload

--- src/components/ImageBox.jsx
+++ src/components/ImageBox.jsx
@@ -3,14 +3,15 @@ import React from 'react'
 class ImageBox extends React.Component {
   constructor(props) {
     super(props)
-    this.state = { loaded: false }
+    this.state = { loaded: false, error: null }
   }
 
   imgLoaded = () => this.setState({ loaded: true })
+  imgError = () => this.setState({ loaded: true, error: 'This is Apple-Map bad!' })
 
   render() {
     const { image, delay } = this.props
-    const { loaded } = this.state
+    const { loaded, error } = this.state
     const style = {
       transition: `opacity .5s ${delay * 0.1}s, height .3s ${delay * 0.1}s`,
       opacity: loaded ? 1 : 0,
@@ -19,7 +20,13 @@ class ImageBox extends React.Component {
     return (
       <div className={`ui ${image.id % 3 === 0 ? 'fade' : `move ${image.id % 4 === 0 ? 'right' : 'up'}`} reveal clipped`} style={style}>
         <div className="visible content">
-          <img src="http://lorempixel.com/g/600/600" className="ui centered medium image" onLoad={this.imgLoaded} />
+          <img
+            src="http://lorempixel.com/g/600/600"
+            className="ui centered medium image"
+            onLoad={this.imgLoaded}
+            onError={this.imgError}
+            alt={error || image.title}
+          />
         </div>
         <div className="hidden content">
           <img src={image.url} className="ui centered medium image" />
